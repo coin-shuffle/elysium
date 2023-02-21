513482da0017453dfbdd2d6fed8ea19d95a4db22
Ivan Lele <ivanlele402@gmail.com>
2023-02-21 16:15:44 +0200

    Initial Commit

---
+++ TrueElysium/TrueElysiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueElysiumApp.swift
+//  TrueElysium
+//
+//  Created by Ivan Lele on 21.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrueElysiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
